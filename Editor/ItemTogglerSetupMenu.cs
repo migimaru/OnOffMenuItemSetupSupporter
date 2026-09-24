e1d28aff76efb203f08aa1a8f7901580c4810301
migimaru <kohei.yoshida0615@gmail.com>
2026-09-24 16:08:16 +0900

    Add edit options for hide when on/off menu items
    Added edit menu items for hiding when on/off functionality.

--- Editor/ItemTogglerSetupMenu.cs
+++ Editor/ItemTogglerSetupMenu.cs
@@ -12,6 +12,12 @@ namespace Migimaru.OnOffMenuItemSetupSupporter.Editor
         private const string HideWhenOffMenuPath =
             "GameObject/On／Off Menu Item Setup Supporter/OFFで非表示";
 
+        private const string EditHideWhenOnMenuPath =
+            "GameObject/On／Off Menu Item Setup Supporter/[編集] ONで非表示";
+
+        private const string EditHideWhenOffMenuPath =
+            "GameObject/On／Off Menu Item Setup Supporter/[編集] OFFで非表示";
+
 
         [MenuItem(HideWhenOnMenuPath, false, 10)]
         private static void OpenHideWhenOn()
@@ -47,17 +53,76 @@ namespace Migimaru.OnOffMenuItemSetupSupporter.Editor
         }
 
 
-        private static bool IsMenuFolderSelected()
+        [MenuItem(EditHideWhenOnMenuPath, false, 20)]
+        private static void OpenEditHideWhenOn()
         {
-            GameObject selectedObject =
-                Selection.activeGameObject;
+            ItemTogglerEditWindow.Open(
+                Selection.activeGameObject,
+                ItemTogglerSetupMode.HideWhenOn
+            );
+        }
 
 
+        [MenuItem(EditHideWhenOnMenuPath, true)]
+        private static bool ValidateEditHideWhenOn()
+        {
+            return HasItemTogglerInSelfOrDescendants(
+                Selection.activeGameObject
+            );
+        }
+
+
+        [MenuItem(EditHideWhenOffMenuPath, false, 21)]
+        private static void OpenEditHideWhenOff()
+        {
+            ItemTogglerEditWindow.Open(
+                Selection.activeGameObject,
+                ItemTogglerSetupMode.HideWhenOff
+            );
+        }
+
+
+        [MenuItem(EditHideWhenOffMenuPath, true)]
+        private static bool ValidateEditHideWhenOff()
+        {
+            return HasItemTogglerInSelfOrDescendants(
+                Selection.activeGameObject
+            );
+        }
+
+
+        internal static bool HasItemTogglerInSelfOrDescendants(
+            GameObject selectedObject
+        )
+        {
             if (selectedObject == null)
             {
                 return false;
             }
 
+            if (
+                selectedObject.GetComponent<MenuFolder>()
+                == null
+            )
+            {
+                return false;
+            }
+
+            return selectedObject
+                .GetComponentsInChildren<ItemToggler>(true)
+                .Length > 0;
+        }
+
+
+        private static bool IsMenuFolderSelected()
+        {
+            GameObject selectedObject =
+                Selection.activeGameObject;
+
+            if (selectedObject == null)
+            {
+                return false;
+            }
 
             return selectedObject.GetComponent<MenuFolder>()
                 != null;
